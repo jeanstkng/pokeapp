31ada37e3796831cfa202f2198a5ec3896a1e84b
JeanStkng <jeancast.jg@gmail.com>
2021-10-11 17:13:36 -0500

    initialized app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { ListComponent } from './components/list/list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
